5566677c5aeb318a62486061ffafecabd4c1b7f8
andreafspeziale <andreaf.speziale@gmail.com>
2017-10-17 16:29:02 +0200

    filter

--- 012/12.js
+++ 012/12.js
@@ -3,6 +3,13 @@
 d3.csv('athletes_sochi.csv', function(err, myData){
     if(err) return
     console.log(myData)
+    
+    var minor24age = myData.filter(function(d){
+        return d.age<=21;
+    })
+    
+    console.log(minor24age)
+    
     var byCountries = d3.nest()
         .key(function(d, i){
             return d.country
@@ -18,4 +25,13 @@ d3.csv('athletes_sochi.csv', function(err, myData){
     })
     
     console.log(byCountries)
+    
+    var byGender = d3.nest()
+        .key(function(d, i){
+            return d.gender
+        })
+        .entries(myData)
+    
+    console.log(byGender)
+    
 })
