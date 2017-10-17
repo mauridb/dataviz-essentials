2df67420f6ecf8e397d94660cca24c6185c716d4
andreafspeziale <andreaf.speziale@gmail.com>
2017-10-17 10:53:02 +0200

    talking about remove exit

---
+++ 008/08.js
@@ -0,0 +1,51 @@
+var dataset = [
+    {age:16},
+    {age:20},
+    {age:30}
+]
+
+console.log(dataset)
+
+// STATES : ENTER - EXIT - UPDATE
+// CHANGE DATAVIZ ACCORDING TO THE DATASET
+
+// DECORATE THE DATAPOINT
+
+function buildMyChart () {
+    var myRects = d3.select('svg')
+        .selectAll('rect')
+        .data(dataset)
+
+    myRects.enter()
+        .append('rect')
+        .attr('x', function(d, i){
+            return i*20
+        })
+        .attr('width', 19)
+        .attr('height', function(d, i){
+                return d.age
+        })
+        .attr('y', function(d, i){
+            return 120 - d.age
+        })
+        .on('mouseenter', function(d, i){
+            d.selected = true
+            d3.select(this)
+                .style('fill', 'violet')
+            console.log(dataset)
+        })
+}
+
+d3.select('#clickme')
+    .on('click', function(){
+        dataset.push({age:Math.random()*100})
+        console.log(dataset)
+        buildMyChart()
+    })
+
+d3.select('#remove')
+    .on('click', function(){
+        dataset.splice(0, 1)
+        console.log(dataset)
+        buildMyChart()
+    })
