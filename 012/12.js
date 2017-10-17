31177c359f68e2691b16a66310ecc2dc97cffc71
andreafspeziale <andreaf.speziale@gmail.com>
2017-10-17 16:22:23 +0200

    dataset pastrocchiare

--- 012/12.js
+++ 012/12.js
@@ -3,10 +3,19 @@
 d3.csv('athletes_sochi.csv', function(err, myData){
     if(err) return
     console.log(myData)
-    var byGender = d3.nest()
+    var byCountries = d3.nest()
+        .key(function(d, i){
+            return d.country
+        })
         .key(function(d, i){
             return d.gender
         })
+        .sortKeys(d3.ascending)
         .entries(myData)
-    console.log(byGender)
+    
+    byCountries.sort(function(a, b){
+        return d3.ascending(a.key, b.key)
+    })
+    
+    console.log(byCountries)
 })
